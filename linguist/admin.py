0d2cdc45c76b7c0d7d6f18ce46e017f3c9064795
Gilles Fabio <gilles@gillesfabio.com>
2014-12-04 14:43:15 +0100

    Add Translation model admin.

--- linguist/admin.py
+++ linguist/admin.py
@@ -3,3 +3,12 @@ from django.contrib import admin
 from django.core.urlresolvers import reverse
 from django.utils.html import format_html
 from django.utils.translation import ugettext_lazy as _
+
+from .models import Translation
+
+
+class TranslationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Translation, TranslationAdmin)
