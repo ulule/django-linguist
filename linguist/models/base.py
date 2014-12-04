7a1d7877a723c3c85ba3bd55f54f243b61f9062c
Gilles Fabio <gilles@gillesfabio.com>
2014-12-04 14:46:20 +0100

    Remove compat import.

--- linguist/models/base.py
+++ linguist/models/base.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 
-from .. import compat
 from ..utils import get_model_string
 
 
