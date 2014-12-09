9ec30c56ba6d285a3b04b4e6a054f44f0532b8e1
Gilles Fabio <gilles@gillesfabio.com>
2014-12-09 17:18:24 +0100

    Add custom queryset and manager.

--- linguist/models/base.py
+++ linguist/models/base.py
@@ -1,9 +1,92 @@
 # -*- coding: utf-8 -*-
+from django.core.cache import cache
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 
 from .. import settings
+
+
+class TranslationQuerySet(models.query.QuerySet):
+
+    def get(self, *args, **kwargs):
+        if self.query.where:
+            return super(TranslationQuerySet, self).get(*args, **kwargs)
+        id_fields = ('identifier', 'object_id', 'language', 'field_name')
+        if len(kwargs) == 1:
+            k = kwargs.keys()[0]
+            if k in ('pk', 'pk__exact', 'id', 'id__exact'):
+                obj = cache.get('linguist_%s' % kwargs.values()[0])
+                if obj is not None:
+                    return obj
+        elif set(id_fields) <= set(kwargs.keys()):
+            values = tuple(kwargs[attr] for attr in id_fields)
+            cache_key = 'linguist_%s_%s_%s_%s' % values
+            obj = cache.get(cache_key)
+            if obj is not None:
+                return obj
+        return super(TranslationQuerySet, self).get(*args, **kwargs)
+
+
+class TranslationManager(models.Manager):
+
+    def get_queryset(self):
+        return TranslationQuerySet(self.model)
+
+    def get_translation(self, identifier, object_id, language, field_name):
+        attrs = dict(
+            identifier=identifier,
+            object_id=object_id,
+            language=language,
+            field_name=field_name)
+        obj = None
+        try:
+            obj = self.get(**attrs)
+        except self.model.DoesNotExist:
+            pass
+        return obj
+
+    def set_translation(self, identifier, object_id, language, field_name, field_value):
+        created = False
+        attrs = dict(
+            identifier=identifier,
+            object_id=object_id,
+            language=language,
+            field_name=field_name,
+            field_value=field_value)
+        try:
+            obj = self.get(**attrs)
+        except self.model.DoesNotExist:
+            obj = self.create(**attrs)
+            created = True
+        if obj.field_value != field_value:
+            obj.field_value = field_value
+            obj.save()
+        return (obj, created)
+
+    def contribute_to_class(self, model, name):
+        models.signals.post_save.connect(self._post_save, sender=model)
+        models.signals.post_delete.connect(self._post_delete, sender=model)
+        setattr(model, '_get_cache_keys', self._get_cache_keys)
+        setattr(model, 'cache_keys', property(self._get_cache_keys))
+        return super(TranslationManager, self).contribute_to_class(model, name)
+
+    def _invalidate_cache(self, instance):
+        for key in instance.cache_keys:
+            cache.set(key, None, 5)
+
+    def _post_save(self, instance, **kwargs):
+        for key in instance.cache_keys:
+            cache.set(key, instance, settings.CACHE_DURATION)
+
+    def _post_delete(self, instance, **kwargs):
+        self._invalidate_cache(instance)
+
+    @staticmethod
+    def _get_cache_keys(obj):
+        id_fields = ('identifier', 'object_id', 'language', 'field_name')
+        values = tuple(getattr(obj, attr) for attr in id_fields)
+        return ('linguist_%s_%s_%s_%s' % values, 'linguist_%s' % obj.id)
 
 
 @python_2_unicode_compatible
@@ -37,6 +120,8 @@ class Translation(models.Model):
         verbose_name=_('field value'),
         null=True,
         help_text=_('The translated content for the field.'))
+
+    objects = TranslationManager()
 
     class Meta:
         abstract = True
