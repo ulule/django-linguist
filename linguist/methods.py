07e4ed16580bb7ba2abcb03a6ca9c676d5c61f8e
Gilles Fabio <gilles@gillesfabio.com>
2014-12-11 15:06:49 +0100

    Add get_current_language, set_current_langage and clear_cache methods.

--- linguist/methods.py
+++ linguist/methods.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .models import Translation
-from .utils import get_cache_key
+from .utils import get_cache_key, get_language
 
 
 def prefetch_translations_for_model(translation_class):
@@ -24,6 +24,26 @@ def prefetch_translations_for_model(translation_class):
     return prefetch_translations
 
 
+def linguist_clear_cache(self):
+    if hasattr(self, '_linguist'):
+        self._linguist = {}
+
+
+def set_current_language(self, language):
+    if not hasattr(self, '_linguist'):
+        self._linguist = {}
+    self._linguist['language'] = language
+
+
+def get_current_language(self):
+    if hasattr(self, '_linguist') and 'language' in self._linguist and self._linguist['language']:
+        return self._linguist['language']
+    return get_language()
+
+
 def add_translation_methods(translation_class):
     model = translation_class.model
+    model.add_to_class('get_current_language', get_current_language)
+    model.add_to_class('set_current_language', set_current_language)
+    model.add_to_class('linguist_clear_cache', linguist_clear_cache)
     model.add_to_class('prefetch_translations', prefetch_translations_for_model(translation_class))
