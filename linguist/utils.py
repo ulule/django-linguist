5b187de714befe932066a42ce863cd04766bba84
Gilles Fabio <gilles@gillesfabio.com>
2014-12-11 11:32:25 +0100

    Add get_language util (and move some functions in utils module).

--- linguist/utils.py
+++ linguist/utils.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.core import exceptions
+from django.utils import six
 from django.utils.importlib import import_module
+from django.utils.encoding import force_text
+from django.utils.functional import lazy
 
 from . import settings
 
@@ -92,3 +95,32 @@ def get_model_string(model_name):
 def get_cache_key(**kwargs):
     keys = ('identifier', 'object_id', 'language', 'field_name')
     return 'linguist_%s_%s_%s_%s' % tuple(kwargs[attr] for attr in keys)
+
+
+def get_language():
+    """
+    Returns an active language code that is guaranteed to be in
+    settings.SUPPORTED_LANGUAGES.
+    """
+    from django.utils.translation import get_language as _get_language
+    lang = _get_language()
+    if lang not in settings.AVAILABLE_LANGUAGES and '-' in lang:
+        lang = lang.split('-')[0]
+    if lang in settings.SUPPORTED_LANGUAGES:
+        return lang
+    return settings.DEFAULT_LANGUAGE
+
+
+def build_localized_field_name(field_name, language):
+    """
+    Build localized field name from ``field_name`` and ``language``.
+    """
+    return '%s_%s' % (field_name, language.replace('-', '_'))
+
+
+def _build_localized_verbose_name(verbose_name, language):
+    """
+    Build localized verbose name from ``verbose_name`` and ``language``.
+    """
+    return force_text('%s [%s]') % (force_text(verbose_name), language)
+build_localized_verbose_name = lazy(_build_localized_verbose_name, six.text_type)
