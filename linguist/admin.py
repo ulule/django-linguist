16bf079d1b139db08988fdb3cc1ff818cecfc12e
Gilles Fabio <gilles@gillesfabio.com>
2014-12-15 14:05:36 +0100

    Add ModelTranslationAdminMixin.

--- linguist/admin.py
+++ linguist/admin.py
@@ -4,7 +4,18 @@ from django.contrib import admin
 from .models import Translation
 
 
+class ModelTranslationAdminMixin(object):
+    """
+    Mixin for model admin classes.
+    """
+    pass
+
+
+
 class TranslationAdmin(admin.ModelAdmin):
+    """
+    Translation model admin options.
+    """
     pass
 
 
