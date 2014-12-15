bd74ee481c4306dda2b3b0125ab0ec49c87341d3
Gilles Fabio <gilles@gillesfabio.com>
2014-12-15 17:46:32 +0100

    Add with_metaclass util.

--- linguist/utils.py
+++ linguist/utils.py
@@ -125,3 +125,17 @@ def _build_localized_verbose_name(verbose_name, language):
     """
     return force_text('%s [%s]') % (force_text(verbose_name), language)
 build_localized_verbose_name = lazy(_build_localized_verbose_name, six.text_type)
+
+
+def with_metaclass(meta, *bases):
+    # Function from python-future and jinja2. License: BSD.
+    # Allow consistent behaviours across all django versions
+    # Also avoids a temporary intermediate class
+    class metaclass(meta):
+        __call__ = type.__call__
+        __init__ = type.__init__
+        def __new__(cls, name, this_bases, d):
+            if this_bases is None:
+                return type.__new__(cls, name, (), d)
+            return meta(name, bases, d)
+    return metaclass('temporary_class', None, {})
