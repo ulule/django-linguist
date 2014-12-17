67ea461219e9542ba57783a390b40ecb8f12223c
Gilles Fabio <gilles@gillesfabio.com>
2014-12-17 12:19:35 +0100

    Clean ModelTranslationFormMixin.

--- linguist/forms.py
+++ linguist/forms.py
@@ -14,18 +14,10 @@ class ModelTranslationFormMixin(object):
     language = None
 
     def __init__(self, *args, **kwargs):
-        current_language = kwargs.pop('_current_language', None)
         super(ModelTranslationFormMixin, self).__init__(*args, **kwargs)
         instance = kwargs.get('instance', None)
-        if self.language is None:
-            if instance:
-                self.language = instance.language
-                return
-            self.language = current_language or get_language()
-
-    def _post_clean(self):
-        self.instance.language = self.language
-        super(ModelTranslationFormMixin, self)._post_clean()
+        if not self.language:
+            self.language = instance.language if instance else get_language()
 
 
 class ModelTranslationForm(ModelTranslationFormMixin, forms.ModelForm):
