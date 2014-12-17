e089ccdf9ee3860398b98a23ce3f15ae20c8ee58
Gilles Fabio <gilles@gillesfabio.com>
2014-12-17 13:49:08 +0100

    Clean forms.

--- linguist/forms.py
+++ linguist/forms.py
@@ -1,23 +1,10 @@
 # -*- coding: utf-8 -*_
 from django import forms
 
-from .utils.i18n import get_language
-
 __all__ = [
-    'ModelTranslationFormMixin',
     'ModelTranslationForm',
 ]
 
 
-class ModelTranslationFormMixin(object):
-
-    def __init__(self, *args, **kwargs):
-        super(ModelTranslationFormMixin, self).__init__(*args, **kwargs)
-        self.language = None
-        instance = kwargs.get('instance', None)
-        if self.language is None:
-            self.language = instance.language if instance else get_language()
-
-
-class ModelTranslationForm(ModelTranslationFormMixin, forms.ModelForm):
+class ModelTranslationForm(forms.ModelForm):
     pass
