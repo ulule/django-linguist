51b71217d057d56f7f25b6473f3abc110ec1b17c
Gilles Fabio <gilles@gillesfabio.com>
2014-12-18 16:38:32 +0100

    Add ManagerMixin (and rename LinguistMixin to ModelMixin).

--- linguist/mixins.py
+++ linguist/mixins.py
@@ -3,7 +3,53 @@ from .models import Translation
 from .utils.i18n import get_cache_key
 
 
-class LinguistMixin(object):
+def set_instance_cache(instance, translations):
+    """
+    Sets Linguist cache for the given instance with data from the given translations.
+    """
+    base_key = dict(identifier=instance._linguist.identifier, object_id=instance.pk)
+    for translation in translations:
+        key = dict(language=translation.language, field_name=translation.field_name)
+        key.update(base_key)
+        cache_key = get_cache_key(**key)
+        if cache_key not in instance._linguist:
+            instance._linguist[cache_key] = translation
+    return instance
+
+
+def get_translation_lookups(instance, fields=None, languages=None):
+    """
+    Returns a dict to pass to Translation.objects.filter().
+    """
+    lookups = dict(identifier=instance.identifier, object_id=instance.pk)
+    if fields is not None:
+        lookups['field_name__in'] = fields
+    if languages is not None:
+        lookups['language__in'] = languages
+    return lookups
+
+
+class ManagerMixin(object):
+    """
+    Linguist Manager Mixin.
+    """
+
+    def with_translations(self, instances, fields=None, languages=None):
+        """
+        Prefetches translations for the given model instances.
+        """
+        if not isinstance(instances, (list, tuple)):
+            instances = [instances]
+
+        for instance in instances:
+            if not isinstance(instance, LinguistMixin):
+                raise Exception('%s must be an instance of LinguistMixin' % instance)
+            lookups = get_translation_lookups(instance, fields, languages)
+            translations = Translation.objects.filter(**lookups)
+            set_instance_cache(instance, translations)
+
+
+class ModelMixin(object):
 
     def clear_translations_cache(self):
         self._linguist.clear()
@@ -19,6 +65,10 @@ class LinguistMixin(object):
     @language.setter
     def language(self, value):
         self._linguist.language = value
+
+    @property
+    def cached_translations_count(self):
+        return len(self._linguist.keys()) - 2
 
     def _get_translations_qs(self, language=None):
         kwargs = dict(identifier=self.identifier, object_id=self.pk)
