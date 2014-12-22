4c954a9a4692363307c64a069b465c984772eced
Gilles Fabio <gilles@gillesfabio.com>
2014-12-22 15:14:12 +0100

    Remove obsolete TranslationFieldMixin and contribute_to_model function.

--- linguist/fields.py
+++ linguist/fields.py
@@ -20,61 +20,6 @@ SUPPORTED_FIELDS = (
 )
 
 
-class TranslationFieldMixin(object):
-    """
-    Common logic for translated and translation fields.
-    """
-
-    def getter_cache(self, instance, field_name, original_field_name, language):
-        if instance is None:
-            raise AttributeError('Can only be accessed via instance')
-
-        kwargs = dict(
-            identifier=instance._linguist.identifier,
-            object_id=instance.pk,
-            language=language,
-            field_name=original_field_name)
-
-        cache_key = get_cache_key(**kwargs)
-        if cache_key in instance._linguist:
-            return instance._linguist[cache_key].field_value
-
-        translation = Translation.objects.get_translation(**kwargs)
-
-        if translation:
-            instance._linguist[cache_key] = translation
-            return translation.field_value
-
-    def setter_cache(self, instance, field_name, original_field_name, language, value):
-        if instance is None:
-            raise AttributeError('Can only be accessed via instance')
-
-        if not instance.pk:
-            return
-
-        kwargs = dict(
-            identifier=instance._linguist.identifier,
-            object_id=instance.pk,
-            language=language,
-            field_name=original_field_name,
-            field_value=value)
-
-        cache_kwargs = copy.copy(kwargs)
-        del cache_kwargs['field_value']
-        cache_key = get_cache_key(**cache_kwargs)
-
-        obj, created = Translation.objects.set_translation(**kwargs)
-        instance._linguist[cache_key] = obj
-
-    def db_type(self, connection):
-        """
-        Returning None will cause Django to exclude this field from the concrete
-        field list (``_meta.concrete_fields``) resulting in the fact that syncdb
-        will skip this field when creating tables in PostgreSQL.
-        """
-        return None
-
-
 class TranslationField(TranslationFieldMixin):
     """
     Translation field.
@@ -92,17 +37,50 @@ class TranslationField(TranslationFieldMixin):
         self.editable = True
 
     def __get__(self, instance, instance_type=None):
-        return self.getter_cache(instance=instance,
-                                 field_name=self.name,
-                                 original_field_name=self.field.name,
-                                 language=self.language)
+        if instance is None:
+            raise AttributeError('Can only be accessed via instance')
+
+        kwargs = dict(
+            identifier=instance._linguist.identifier,
+            object_id=instance.pk,
+            language=self.language,
+            field_name=self.field.name)
+
+        cache_key = get_cache_key(**kwargs)
+        if cache_key in instance._linguist:
+            return instance._linguist[cache_key].field_value
+
+        translation = Translation.objects.get_translation(**kwargs)
+
+        if translation:
+            instance._linguist[cache_key] = translation
+            return translation.field_value
 
     def __set__(self, instance, value):
-        self.setter_cache(instance=instance,
-                          field_name=self.name,
-                          original_field_name=self.field.name,
-                          language=self.language,
-                          value=value)
+        if instance is None:
+            raise AttributeError('Can only be accessed via instance')
+
+        kwargs = dict(
+            identifier=instance._linguist.identifier,
+            object_id=instance.pk,
+            language=self.language,
+            field_name=self.field.name,
+            field_value=value)
+
+        cache_kwargs = copy.copy(kwargs)
+        del cache_kwargs['field_value']
+        cache_key = get_cache_key(**cache_kwargs)
+
+        obj, created = Translation.objects.set_translation(**kwargs)
+        instance._linguist[cache_key] = obj
+
+    def db_type(self, connection):
+        """
+        Returning None will cause Django to exclude this field from the concrete
+        field list (``_meta.concrete_fields``) resulting in the fact that syncdb
+        will skip this field when creating tables in PostgreSQL.
+        """
+        return None
 
 
 class CacheDescriptor(dict):
@@ -127,10 +105,3 @@ class CacheDescriptor(dict):
     @identifier.setter
     def identifier(self, value):
         self['identifier'] = value
-
-
-def contribute_to_model(translation_class):
-    """
-    Temporarly adds translation class to model to pass it to metaclass.
-    """
-    model.add_to_class('linguist_translation_class', translation_class)
