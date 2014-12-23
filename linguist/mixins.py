12546cd61673629ce78796ee19aa436233987ea9
Gilles Fabio <gilles@gillesfabio.com>
2014-12-23 16:58:33 +0100

    Model mixin: add get_translations_for_save() method.

--- linguist/mixins.py
+++ linguist/mixins.py
@@ -14,15 +14,25 @@ from .utils.i18n import (get_cache_key,
 
 def set_instance_cache(instance, translations):
     """
-    Sets Linguist cache for the given instance with data from the given translations.
+    Sets Linguist cache for the given instance.
     """
-    base_key = dict(identifier=instance._linguist.identifier, object_id=instance.pk)
     for translation in translations:
-        key = dict(language=translation.language, field_name=translation.field_name)
-        key.update(base_key)
-        cache_key = get_cache_key(**key)
+
+        cache_key = get_cache_key(**dict(
+            identifier=instance.linguist_identifier,
+            object_id=instance.pk,
+            language=translation.language,
+            field_name=translation.field_name))
+
         if cache_key not in instance._linguist:
-            instance._linguist[cache_key] = translation
+            instance._linguist[cache_key] = dict(
+                    pk=translation.pk,
+                    object_id=instance.pk,
+                    identifier=instance.linguist_identifier,
+                    language=translation.language,
+                    field_name=translation.field_name,
+                    field_value=translation.field_value)
+
     return instance
 
 
@@ -30,32 +40,17 @@ def get_translation_lookups(instance, fields=None, languages=None):
     """
     Returns a dict to pass to Translation.objects.filter().
     """
-    lookups = dict(identifier=instance.identifier, object_id=instance.pk)
+    lookups = dict(
+        identifier=instance.linguist_identifier,
+        object_id=instance.pk)
+
     if fields is not None:
         lookups['field_name__in'] = fields
+
     if languages is not None:
         lookups['language__in'] = languages
+
     return lookups
-
-
-def get_translations_for_save(instance):
-    """
-    Takes a given instance and returns a list of tuples containing translations
-    lookup and cache key for ``Translation`` manager.
-    """
-    translations = []
-    for field in instance.translatable_fields:
-        lookup = dict(
-            identifier=instance._linguist.identifier,
-            object_id=instance.pk,
-            language=instance.language,
-            field_name=field,
-            field_value=getattr(instance, field, ''))
-        cache_key = copy.copy(lookup)
-        del cache_key['field_value']
-        cache_key = get_cache_key(**cache_key)
-        translations.append((lookup, cache_key))
-    return translations
 
 
 class ManagerMixin(object):
@@ -63,21 +58,24 @@ class ManagerMixin(object):
     Linguist Manager Mixin.
     """
 
-    def save_translations(self):
-        instances = self.get_queryset()
-
     def with_translations(self, fields=None, languages=None):
         """
         Prefetches translations.
         """
-        instances = self.get_queryset()
-        for instance in instances:
+        for instance in self.get_queryset():
             lookups = get_translation_lookups(instance, fields, languages)
             translations = Translation.objects.filter(**lookups)
             set_instance_cache(instance, translations)
 
 
 class ModelMixin(object):
+
+    @property
+    def linguist_identifier(self):
+        """
+        Returns Linguist's identifier for this model.
+        """
+        self._linguist.identifier
 
     @property
     def language(self):
@@ -106,7 +104,7 @@ class ModelMixin(object):
         Returns available languages.
         """
         return (Translation.objects
-                           .filter(identifier=self.identifier, object_id=self.pk)
+                           .filter(identifier=self.linguist_identifier, object_id=self.pk)
                            .values_list('language', flat=True)
                            .distinct()
                            .order_by('language'))
@@ -117,14 +115,29 @@ class ModelMixin(object):
         """
         self._linguist.clear()
 
+    def get_translations_for_save(self):
+        """
+        Returns translation instances to save for the given model instance.
+        """
+        cache_keys = []
+
+        for translatable_field in self.translatable_fields:
+            for language_code, language_name in settings.SUPPORTED_LANGUAGES:
+                cache_key_kwargs = dict(
+                    identifier=self.linguist_identifier,
+                    object_id=self.pk,
+                    language=language_code,
+                    field_name=translatable_field)
+                cache_key = get_cache_key(**cache_key_kwargs)
+                cache_keys.append(cache_key)
+
+        return [self._linguist[k] for k in cache_keys if k in self._linguist]
+
     def save_translations(self):
         """
         Saves translations in the database.
         """
-        translations = get_translations_for_save(self)
-        for lookup, cache_key in translations:
-            obj, created = Translation.objects.set_translation(**lookup)
-            self._linguist[cache_key] = obj
+        Translation.objects.save_cached(self.get_translations_for_save())
 
     def save(self, *args, **kwargs):
         """
