18afd13eb4c3c00089faabdc81f2498b679c9ab2
Gilles Fabio <gilles@gillesfabio.com>
2014-12-29 12:57:50 +0100

    Refactor CachedTranslation class.

--- linguist/cache.py
+++ linguist/cache.py
@@ -1,100 +1,60 @@
 # -*- coding: utf-8 -*-
+from . import utils
 
 
 class CachedTranslation(object):
 
     def __init__(self, *args, **kwargs):
+        from .models import Translation
 
         # Is a new instance (not saved in db)
         self.is_new = kwargs.get('is_new', True)
 
-        # Translation.identifier
-        self.identifier = kwargs.get('identifier', None)
+        # Translation model fields
+        self.fields = Translation._meta.get_all_field_names()
+        self.fields.remove('id')
 
-        # Translation.object_id
-        self.object_id = kwargs.get('object_id', None)
+        # Model instances
+        self.instances = ['instance', 'translation']
 
-        # Model.language
-        self.language = kwargs.get('language', None)
+        # Set all attributes to None by default
+        attrs = self.fields + self.instances
+        for attr in attrs:
+            setattr(self, attr, None)
 
-        # Translation.field_name
-        self.field_name = kwargs.get('field_name', None)
+        self.__dict__.update(**kwargs)
 
-        # Translation.field_value
-        self.field_value = kwargs.get('field_value', None)
+        if self.instance is not None:
+            is_new = bool(self.instance.pk is None)
+            instance_pk = self.instance.pk if not is_new else utils.make_temp_id(self.instance)
+            self.identifier = self.instance.linguist_identifier
+            self.object_id = instance_pk
 
-        instance = kwargs.get('instance', None)
-        translation = kwargs.get('translation', None)
-
-        if instance is not None:
-            is_new = bool(instance.pk is None)
-            instance_pk = instance.pk if not is_new else 'new-%s' % id(instance)
-            self.identifier = instance.linguist_identifier
-            self.object_id = '%s' % instance_pk
-
-        if translation is not None:
-            self.language = translation.language
-            self.field_name = translation.field_name
-            self.field_value = translation.field_value
+        if self.translation is not None:
+            for attr in ('language', 'field_name', 'field_value'):
+                setattr(self, attr, getattr(self.translation, attr))
 
     @property
     def attrs(self):
         """
         Returns Translation attributes to pass as kwargs for creating or updating objects.
         """
-        return {
-            'identifier': self.identifier,
-            'object_id': self.object_id,
-            'language': self.language,
-            'field_name': self.field_name,
-            'field_value': self.field_value,
-        }
+        return dict((k, getattr(self, k)) for k in self.fields)
 
     @property
     def lookup(self):
         """
         Returns Translation lookup to use for filter method.
         """
-        lookup = {
-            'identifier': self.identifier,
-            'object_id': self.object_id,
-        }
-
+        lookup = {'identifier': self.identifier, 'object_id': self.object_id}
         if self.language is not None:
             lookup['language'] = self.language
-
         return lookup
 
-    def update_from_object(self, obj):
+    def from_object(self, obj):
         """
         Updates values from the given object.
         """
         fields = ('identifier', 'object_id', 'language', 'field_name', 'field_value')
         for field in fields:
             setattr(self, field, getattr(obj, field))
-
-
-def make_cache_key(instance, **kwargs):
-    """
-    Generates translation cache key.
-    """
-    translation = kwargs.get('translation', None)
-    language = kwargs.get('language', None)
-    field_name = kwargs.get('field_name', None)
-
-    if translation is None:
-        if not (language and field_name):
-            raise Exception("You must set language and field name")
-
-    if translation is not None:
-        language = translation.language
-        field_name = translation.field_name
-
-    is_new = bool(instance.pk is None)
-    instance_pk = instance.pk if not is_new else 'new-%s' % id(instance)
-
-    return '%s_%s_%s_%s' % (
-        instance.linguist_identifier,
-        instance_pk,
-        language,
-        field_name)
