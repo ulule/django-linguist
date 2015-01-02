321c1798410f717e722bb43d6530499cb783ace5
Gilles Fabio <gilles@gillesfabio.com>
2015-01-02 15:07:43 +0100

    Implement ModelTranslationFormMixin.

--- linguist/forms.py
+++ linguist/forms.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*_
 from django import forms
 
+from . import utils
+from .models import Translation
+
 __all__ = [
+    'ModelTranslationFormMixin',
     'ModelTranslationForm',
 ]
 
 
-class ModelTranslationForm(forms.ModelForm):
+class ModelTranslationFormMixin(object):
+    """
+    Base methods added to ``ModelTranslationForm``.
+    """
+    language = None
+
+    def __init__(self, *args, **kwargs):
+
+        super(ModelTranslationFormMixin, self).__init__(*args, **kwargs)
+
+        instance = kwargs.get('instance', None)
+
+        if self.language is None:
+            self.language = utils.get_language()
+
+        if instance is not None:
+            self.instance.language = self.language
+
+
+class ModelTranslationForm(ModelTranslationFormMixin, forms.ModelForm):
     pass
