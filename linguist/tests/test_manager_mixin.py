04bbd3670e34e9e123b30a195a9d2119597b3419
Gilles Fabio <gilles@gillesfabio.com>
2015-01-06 18:10:40 +0100

    Drastically improve tests (num queries).

--- linguist/tests/test_manager_mixin.py
+++ linguist/tests/test_manager_mixin.py
@@ -15,84 +15,213 @@ class ManagerMixinTest(BaseTestCase):
         self.create_registry()
 
     def test_with_translations(self):
+        # Be sure we have the method
         self.assertTrue(hasattr(FooModel.objects, 'with_translations'))
+
+        # Create English content
         self.instance.language = 'en'
         self.instance.title = 'Hello'
+
+        # Create French content
         self.instance.language = 'fr'
         self.instance.title = 'Bonjour'
-        self.instance.save()
 
+        # Persist!
+        #
+        # 1 - INSERT INTO foomodel
+        # 2 - SAVEPOINT
+        # 3 - INSERT INTO translation
+        # 4 - RELEASE SAVEPOINT
+        with self.assertNumQueries(4):
+            self.instance.save()
+
+        # Titles are now cached
+        with self.assertNumQueries(0):
+            self.instance.language = 'en'
+            en_title = '%s' % self.instance.title  # noqa
+            self.instance.language = 'fr'
+            fr_title = '%s' % self.instance.title  # noqa
+
+        # Preload translations without clearing the cache
+        #
+        # 1 - SELECT ALL foomodel
+        # 2 - SELECT IN translation
+        with self.assertNumQueries(2):
+            FooModel.objects.with_translations()
+
+        # Clear cache
         self.instance.clear_translations_cache()
         self.assertEqual(self.instance.cached_translations_count, 0)
 
-        FooModel.objects.with_translations()
+        # Preload translations with cache cleared
+        #
+        # 1 - SELECT ALL foomodel
+        # 2 - SELECT IN translation
+        with self.assertNumQueries(2):
+            FooModel.objects.with_translations()
 
-        self.assertEqual(self.instance.cached_translations_count, 2)
+        # Database should be not hit
+        with self.assertNumQueries(0):
+            self.assertEqual(self.instance.cached_translations_count, 2)
 
     def test_with_translations_args(self):
-        # Let's create an English post
+        # Create English content
         self.instance.language = 'en'
         self.instance.title = 'Hello'
         self.instance.excerpt = 'This is the excerpt.'
         self.instance.body = 'This is the body.'
 
-        # Let's create a French post
+        # Create French content
         self.instance.language = 'fr'
         self.instance.title = 'Bonjour'
         self.instance.excerpt = 'Ceci est la description'
         self.instance.body = 'Corps'
 
         # Persist!
-        self.instance.save()
+        #
+        # 1 - INSERT INTO foomodel
+        # 2 - SAVEPOINT
+        # 3 - INSERT INTO translation
+        # 4 - RELEASE SAVEPOINT
+        with self.assertNumQueries(4):
+            self.instance.save()
 
-        # Clear cache (remove all translations previously cached)
+        # Preload translations without clearing the cache
+        #
+        # 1 - SELECT ALL foomodel
+        # 2 - SELECT IN translation
+        with self.assertNumQueries(2):
+            FooModel.objects.with_translations()
+
+        # Clear cache
         self.instance.clear_translations_cache()
         self.assertEqual(self.instance.cached_translations_count, 0)
 
         # If we just want the title field
-        FooModel.objects.with_translations(field_names=('title',))
+        #
+        # 1 - SELECT ALL foomodel
+        # 2 - SELECT IN translation
+        with self.assertNumQueries(2):
+            FooModel.objects.with_translations(field_names=('title',))
+
+        # Cache has been cleared and we got now the two titles
         self.assertEqual(self.instance.cached_translations_count, 2)
 
-        self.assertTrue(self.instance._linguist.translations['title']['fr'])
-        self.assertTrue(self.instance._linguist.translations['title']['en'])
+        # Verify dict
+        with self.assertNumQueries(0):
+            self.assertTrue(self.instance._linguist.translations['title']['fr'])
+            self.assertTrue(self.instance._linguist.translations['title']['en'])
 
-        # Clear cache (remove all translations previously cached)
+        # Clear cache
         self.instance.clear_translations_cache()
         self.assertEqual(self.instance.cached_translations_count, 0)
 
         # If we just want the title and body (excluding excerpt)
-        FooModel.objects.with_translations(field_names=('title', 'body'))
+        #
+        # 1 - SELECT ALL foomodel
+        # 2 - SELECT IN translation
+        with self.assertNumQueries(2):
+            FooModel.objects.with_translations(field_names=('title', 'body'))
 
+        # Cached has been cleared. We should have title/body for each language
         self.assertEqual(self.instance.cached_translations_count, 4)
+
+        # Verify dict
         self.assertTrue(self.instance._linguist.translations['title']['fr'])
         self.assertTrue(self.instance._linguist.translations['body']['fr'])
         self.assertTrue(self.instance._linguist.translations['title']['en'])
         self.assertTrue(self.instance._linguist.translations['body']['en'])
 
-        # Clear cache (remove all translations previously cached)
+        # Clear cache
         self.instance.clear_translations_cache()
         self.assertEqual(self.instance.cached_translations_count, 0)
 
         # If we just want title and excerpt and only english
-        FooModel.objects.with_translations(field_names=('title', 'excerpt'), languages=('en',))
+        #
+        # 1 - SELECT ALL foomodel
+        # 2 - SELECT IN translation
+        with self.assertNumQueries(2):
+            FooModel.objects.with_translations(field_names=('title', 'excerpt'), languages=('en',))
+
+        # Cache has been cleared. We should have title/excerpt for English only.
         self.assertEqual(self.instance.cached_translations_count, 2)
+
+        # Verify dict
         self.assertTrue(self.instance._linguist.translations['title']['en'])
         self.assertTrue(self.instance._linguist.translations['excerpt']['en'])
 
-        # Clear cache (remove all translations previously cached)
+        # Clear cache
         self.instance.clear_translations_cache()
         self.assertEqual(self.instance.cached_translations_count, 0)
 
         # If we just want title, excerpt, body and only french
-        FooModel.objects.with_translations(field_names=('title', 'excerpt', 'body'), languages=('fr',))
+        #
+        # 1 - SELECT ALL foomodel
+        # 2 - SELECT IN translation
+        with self.assertNumQueries(2):
+            FooModel.objects.with_translations(field_names=('title', 'excerpt', 'body'), languages=('fr',))
+
+        # Cache has been cleared. We should have title/excerpt/body for French only
         self.assertEqual(self.instance.cached_translations_count, 3)
 
+        # Verify dict
         self.assertTrue(self.instance._linguist.translations['title']['fr'])
         self.assertTrue(self.instance._linguist.translations['excerpt']['fr'])
         self.assertTrue(self.instance._linguist.translations['body']['fr'])
 
         # If we just want title in english and french
-        FooModel.objects.with_translations(field_names=('title',), languages=('fr', 'en'))
+        #
+        # 1 - SELECT ALL foomodel
+        # 2 - SELECT IN translation
+        with self.assertNumQueries(2):
+            FooModel.objects.with_translations(field_names=('title',), languages=('fr', 'en'))
+
+        # Cache has been cleared. We should have titles for French and English
         self.assertEqual(self.instance.cached_translations_count, 2)
+
+        # Verify dict
         self.assertTrue(self.instance._linguist.translations['title']['fr'])
         self.assertTrue(self.instance._linguist.translations['title']['en'])
+
+    def test_without_prefetching(self):
+        # Create English content
+        self.instance.language = 'en'
+        self.instance.title = 'Hello'
+
+        # Create French content
+        self.instance.language = 'fr'
+        self.instance.title = 'Bonjour'
+
+        # Persist!
+        #
+        # 1 - INSERT INTO foomodel
+        # 2 - SAVEPOINT
+        # 3 - INSERT INTO translation
+        # 4 - RELEASE SAVEPOINT
+        with self.assertNumQueries(4):
+            self.instance.save()
+
+        # Clear cache
+        self.instance.clear_translations_cache()
+        self.assertEqual(self.instance.cached_translations_count, 0)
+
+        # Because we don't use with_translations() here, each __get__ should
+        # hit the database:
+        #
+        # 1 - Fetch title en
+        # 2 - Fetch title fr
+        with self.assertNumQueries(2):
+            self.instance.language = 'en'
+            en_title = '%s' % self.instance.title  # noqa
+            self.instance.language = 'fr'
+            fr_title = '%s' % self.instance.title  # noqa
+
+        # Be sure titles are now in cache
+        self.assertEqual(self.instance.cached_translations_count, 2)
+
+        # Let's try again. Database should not be hit
+        with self.assertNumQueries(0):
+            self.instance.language = 'en'
+            en_title = '%s' % self.instance.title  # noqa
+            self.instance.language = 'fr'
+            fr_title = '%s' % self.instance.title  # noqa
