267738968fa940ab72d912af2c6e975b24155abd
Gilles Fabio <gilles@gillesfabio.com>
2015-01-14 17:55:09 +0100

    Remove autodiscover.

--- linguist/models/__init__.py
+++ linguist/models/__init__.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*-
-import django
-
 from ..utils.models import load_class
 from .. import settings
 
 
 Translation = load_class(settings.TRANSLATION_MODEL)
-
-
-if django.VERSION < (1, 7):
-    from .. import autodiscover
-    autodiscover()
 
 
 from ..signals import *  # noqa
