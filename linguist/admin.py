6a95675e9f67a5db1e31844a7b557e757f01ecd0
Gilles Fabio <gilles@gillesfabio.com>
2015-01-15 14:52:25 +0100

    Remove obsolete templates and statics.

--- linguist/admin.py
+++ linguist/admin.py
@@ -2,12 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django.forms import Media
-from django.utils.functional import lazy
 from django.utils.translation import ugettext_lazy as _
-from django.utils import six
-
-from . import utils
 
 from .models import Translation as LinguistTranslationModel
 
@@ -16,35 +11,7 @@ __all__ = [
 ]
 
 
-_lazy_select_template_name = lazy(utils.select_template_name, six.text_type)
-
-
 class ModelTranslationAdmin(admin.ModelAdmin):
-
-    @property
-    def media(self):
-        """
-        Add Linguist media files.
-        """
-        return super(ModelTranslationAdmin, self).media + Media(js=['linguist/admin/linguist.js'])
-
-    @property
-    def change_form_template(self):
-        """
-        Overrides ``admin/change_form.html`` template.
-        """
-        return 'admin/linguist/change_form.html'
-
-    def get_change_form_base_template(self):
-        """
-        Overrides base change form template.
-        """
-        opts = self.model._meta
-        app_label = opts.app_label
-        return _lazy_select_template_name((
-            "admin/{0}/{1}/change_form.html".format(app_label, opts.object_name.lower()),
-            "admin/{0}/change_form.html".format(app_label),
-            "admin/change_form.html"))
 
     def get_available_languages(self, obj):
         """
@@ -61,31 +28,6 @@ class ModelTranslationAdmin(admin.ModelAdmin):
 
     languages_column.allow_tags = True
     languages_column.short_description = _('Languages')
-
-    def add_view(self, request, form_url='', extra_context=None):
-        """
-        Adds default language and translatable fields to template context.
-        """
-        extra_context = extra_context or {}
-        extra_context['default_language'] = self.model._linguist.default_language
-        extra_context['translatable_fields'] = self.model._linguist.fields
-        return super(ModelTranslationAdmin, self).add_view(
-            request=request,
-            form_url=form_url,
-            extra_context=extra_context)
-
-    def change_view(self, request, object_id, form_url='', extra_context=None):
-        """
-        Adds default language and translatable fields to template context.
-        """
-        extra_context = extra_context or {}
-        extra_context['default_language'] = self.model._linguist.default_language
-        extra_context['translatable_fields'] = self.model._linguist.fields
-        return super(ModelTranslationAdmin, self).change_view(
-            request=request,
-            object_id=object_id,
-            form_url=form_url,
-            extra_context=extra_context)
 
 
 class LinguistTranslationModelAdmin(admin.ModelAdmin):
