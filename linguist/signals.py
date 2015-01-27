c136d509efefa31d11fe193f83f5c3e01b2831cf
Gilles Fabio <gilles@gillesfabio.com>
2015-01-27 18:24:06 +0100

    Add decider support in signals.

--- linguist/signals.py
+++ linguist/signals.py
@@ -2,7 +2,6 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from .models import Translation
 from .mixins import ModelMixin
 
 
@@ -12,5 +11,5 @@ def delete_translations(sender, instance, **kwargs):
     Deletes related instance's translations when instance is deleted.
     """
     if issubclass(sender, (ModelMixin, )):
-        Translation.objects.filter(identifier=instance.linguist_identifier,
-                                   object_id=instance.pk).delete()
+        instance._linguist.decider.objects.filter(identifier=instance.linguist_identifier,
+                                                  object_id=instance.pk).delete()
