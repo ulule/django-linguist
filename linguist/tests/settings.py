8d864f60892c83d07b90963cae9045c82183996e
Gilles Fabio <gilles@gillesfabio.com>
2015-12-15 13:22:17 +0300

    Use PostgreSQL for tests.

--- linguist/tests/settings.py
+++ linguist/tests/settings.py
@@ -4,12 +4,11 @@ import django
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '',
-        'USER': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_linguist',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '',
-        'PORT': '',
     }
 }
 
