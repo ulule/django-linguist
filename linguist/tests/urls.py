3688ba64c206277ea1bc41d016dcced4f192fadd
Gilles Fabio <gilles@gillesfabio.com>
2015-12-16 11:40:14 +0300

    Tests views.

---
+++ linguist/tests/urls.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.core import serializers
+from django.forms.models import model_to_dict
+from django.http import JsonResponse
+from .models import FooModel
+
+
+def home(request):
+    obj = FooModel.objects.first()
+    return JsonResponse({'title': obj.title})
+
+
+urlpatterns = patterns('',
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',  home, name='home')
+)
