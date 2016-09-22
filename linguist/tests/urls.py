d2ae5f83c77d291ef9ac65f9a6aff3ceeeb304c6
Alexandre Varas <alexandre@strip.ly>
2016-09-22 16:49:58 +0200

    Added support for Python 3.5 and Django 1.10

--- linguist/tests/urls.py
+++ linguist/tests/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.http import JsonResponse
 
@@ -12,8 +12,8 @@ def home(request):
     return JsonResponse({'title': obj.title})
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',  home, name='home')
-)
+]
